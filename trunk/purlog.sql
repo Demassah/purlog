ee351be686905c40630023ddb8e5d222d3776206
Iqbal4692@gmail.com <Iqbal4692@gmail.com@71c10aaa-1455-b3bc-a450-06e7af52a3db>
2014-12-01 08:43:14 +0000

    db

--- trunk/purlog.sql
+++ trunk/purlog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2014 at 06:49 PM
+-- Generation Time: Dec 01, 2014 at 12:42 AM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `ref_barang` (
   `nama_barang` varchar(30) NOT NULL,
   `jumlah` int(5) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `ref_barang`
@@ -41,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `ref_barang` (
 
 INSERT INTO `ref_barang` (`id`, `id_kategori`, `id_sub_kategori`, `kode_barang`, `nama_barang`, `jumlah`) VALUES
 (1, 2, 2, '001', 'Busi', 100),
-(2, 2, 2, '002', 'Ban', 25);
+(2, 2, 2, '101', 'Asd', 25),
+(4, 3, 4, '43', 'adsl', 23);
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `ref_kategori` (
 
 INSERT INTO `ref_kategori` (`id_kategori`, `nama_kategori`) VALUES
 (2, 'Consumables'),
-(3, 'Not Consumables');
+(3, 'Not  Consumables');
 
 -- --------------------------------------------------------
 
@@ -134,19 +135,19 @@ CREATE TABLE IF NOT EXISTS `sys_menu` (
 INSERT INTO `sys_menu` (`menu_id`, `menu_group`, `menu_name`, `menu_parent`, `url`, `position`, `hide`, `icon_class`, `policy`) VALUES
 (1, 'Administrator', 'Administrator', 0, '#', 99, 0, 'icon-administrator', 'ACCESS;'),
 (2, 'Administrator', 'Otoritas Menu', 1, 'otoritas', 1, 0, 'icon-otoritas', 'ACCESS;ADD;EDIT;DELETE;'),
-(3, 'Administrator', 'Pengguna', 1, 'pengguna', 2, 0, 'icon-pengguna', 'ACCESS;ADD;EDIT;DELETE;'),
+(3, 'Administrator', 'Pengguna', 1, 'pengguna', 2, 0, 'icon-user', 'ACCESS;ADD;EDIT;DELETE;'),
 (4, 'Administrator', 'Departemen', 1, 'departement', 3, 0, 'icon-departement', 'ACCESS;ADD;EDIT;DELETE;'),
 (5, 'Master Data', 'Master Data', 0, '#', 1, 0, 'icon-master', 'ACCESS;'),
 (6, 'Master Data', 'Kategori', 5, 'kategori', 2, 0, 'icon-kategori', 'ACCESS;ADD;EDIT;DELETE;'),
-(7, 'Master Data', 'Sub Kategori', 5, 'sub_kategori', 3, 0, 'icon-sub_kategori', 'ACCESS;ADD;EDIT;DELETE;'),
-(8, 'Master Data', 'Barang', 5, 'barang', 3, 0, 'icon-barang', 'ACCESS;ADD;EDIT;DELETE;'),
+(7, 'Master Data', 'Sub Kategori', 5, 'sub_kategori', 3, 0, 'icon-subkateg', 'ACCESS;ADD;EDIT;DELETE;'),
+(8, 'Master Data', 'Barang', 5, 'barang', 4, 0, 'icon-barang', 'ACCESS;ADD;EDIT;DELETE;PRINT;IMPORT;'),
 (9, 'Rancang', 'Rancang', 0, '#', 1, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (10, 'Rancang', 'Request Order', 9, '#', 2, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (11, 'Rancang', 'Detail RO', 9, '#', 3, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (12, 'Rancang', 'Request Order Selected', 9, '#', 4, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (13, 'Rancang', 'Detail ROS', 9, '#', 5, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
-(14, 'Rancang', 'Picking ROS', 9, '#', 6, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
-(15, 'Rancang', 'Detail PROS', 9, '#', 7, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
+(14, 'Rancang', 'Picking ROS', 9, 'picking', 6, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
+(15, 'Rancang', 'Detail PROS', 9, 'detail_picking', 7, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (16, 'Rancang', 'Shipment Request Order', 9, '#', 8, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (17, 'Rancang', 'SRO Detail', 9, '#', 9, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (18, 'Rancang', 'Purchase Request', 9, '#', 10, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
@@ -161,7 +162,8 @@ INSERT INTO `sys_menu` (`menu_id`, `menu_group`, `menu_name`, `menu_parent`, `ur
 (27, 'Rancang', 'Delivery Order', 9, '#', 19, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (28, 'Rancang', 'Detail DO', 9, '#', 20, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
 (29, 'Rancang', 'Berita Acara Pengembalian', 9, '#', 21, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
-(30, 'Rancang', 'Berita Acara Pengembalian Pengiriman', 9, '#', 22, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;');
+(30, 'Rancang', 'Berita Acara Pengembalian Pengiriman', 9, '#', 22, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;'),
+(31, 'Rancang', 'Request Order Logistic', 9, 'ro_logistic', 23, 0, NULL, 'ACCESS;ADD;EDIT;DELETE;DETAIL;');
 
 -- --------------------------------------------------------
 
@@ -200,7 +202,7 @@ CREATE TABLE IF NOT EXISTS `sys_user_access` (
   `user_level_id` smallint(6) NOT NULL DEFAULT '0',
   `policy` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_access_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=726 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=727 ;
 
 --
 -- Dumping data for table `sys_user_access`
@@ -214,14 +216,14 @@ INSERT INTO `sys_user_access` (`user_access_id`, `menu_id`, `user_level_id`, `po
 (700, 5, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (701, 7, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (702, 6, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
-(703, 8, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
+(703, 8, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;'),
 (704, 9, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (705, 10, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (706, 11, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (707, 12, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (708, 13, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
-(709, 14, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
-(710, 15, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
+(709, 14, 1, 'ACCESS;DETAIL;'),
+(710, 15, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;'),
 (711, 16, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (712, 17, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (713, 18, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
@@ -236,7 +238,8 @@ INSERT INTO `sys_user_access` (`user_access_id`, `menu_id`, `user_level_id`, `po
 (722, 27, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (723, 28, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
 (724, 29, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
-(725, 30, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;');
+(725, 30, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;PRINT;'),
+(726, 31, 1, 'ACCESS;ADD;DETAIL;EDIT;DELETE;');
 
 -- --------------------------------------------------------
 
