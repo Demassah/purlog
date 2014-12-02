7d652663d69bbf571c0f09492075936a29ec6fe7
Demassah <Demassah@gmail.com>
2014-12-02 04:05:34 +0000

    update ros dan ro

---
+++ application/controllers/request_order.php
@@ -0,0 +1,89 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class request_order extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		//$this->load->model('mdl_kategori');
+		//$this->output->enable_profiler(TRUE);
+	}
+	
+	function index(){
+		$this->load->view('request_order/index');
+	}
+
+	function grid(){
+		$data = $this->mdl_barang->getdata();
+		echo $this->mdl_barang->togrid($data['row_data'], $data['row_count']);
+	}
+		
+	function add(){
+		$data['kode'] = '';
+	
+		
+		$this->load->view('request_order/form', $data);
+	}
+	
+	function edit($kode){
+		$r = $this->mdl_barang->getdataedit($kode);
+		
+    $data['id_kategori'] = $r->row()->id_kategori;
+    $data['id_sub_kategori'] = $r->row()->id_sub_kategori;
+    $data['kode_barang'] = $r->row()->kode_barang;
+    $data['nama_barang'] = $r->row()->nama_barang;
+    $data['jumlah'] = $r->row()->jumlah;
+		$data['kode'] = $kode;
+		
+		$this->load->view('request_order/form', $data);
+	}
+	
+	// function save($aksi){
+	// 	# init
+	// 	$status = "";
+	// 	$result = false;
+	// 	$data['pesan_error'] = '';
+		
+	// 	# get post data
+	// 	foreach($_POST as $key => $value){
+	// 		$data[$key] = $value;
+	// 	}
+		
+	// 	# rules validasi form
+	// 	$this->form_validation->set_rules("id_kategori", 'Kategori', 'trim|required|xss_clean');
+	// 	$this->form_validation->set_rules("id_sub_kategori", 'Sub Kategori', 'trim|required|xss_clean');
+	// 	$this->form_validation->set_rules("kode_barang", 'Kode Barang', 'trim|required|xss_clean');
+	// 	$this->form_validation->set_rules("nama_barang", 'Nama Barang', 'trim|required|xss_clean');
+
+	// 	# message rules
+	// 	$this->form_validation->set_message('required', 'Field %s harus diisi.');
+
+	// 	$data['pesan_error'] = '';
+	// 	if ($this->form_validation->run() == FALSE){
+	// 		//$data["pesan_error"] .= trim(form_error('kd_barang',' ',' '))==''?'':form_error('kd_barang',' ',' ').'<br>';
+	// 		$data["pesan_error"] .= trim(validation_errors(' ',' '))==''?'':validation_errors(' ',' ');
+	// 	}else{
+	// 		if($aksi=="add"){ // add
+	// 			$result = $this->mdl_barang->InsertOnDb($data);
+	// 		}else { // edit
+	// 			$result=$this->mdl_barang->UpdateOnDb($data);
+	// 		}
+	// 	}
+		
+	// 	if($result){
+	// 		echo json_encode(array('success'=>true));
+	// 	}else{
+	// 		echo json_encode(array('msg'=>$data['pesan_error']));
+	// 	}
+	// }
+	
+	// function delete($kode){
+	// 	$result = $this->mdl_barang->DeleteOnDb($kode);
+	// 	if ($result){
+	// 		echo json_encode(array('success'=>true));
+	// 	} else {
+	// 		echo json_encode(array('msg'=>'Data gagal dihapus'));
+	// 	}
+	// }
+}
+/* End of file request_order.php */
+/* Location: ./application/controllers/request_order.php */
--- application/views/request_order_selected/index.php
+++ application/views/request_order_selected/index.php
@@ -1,21 +1,22 @@
-	var url;
+<script type="text/javascript">
+		var url;
 	var base_url = $('#base').val();
-	var site = base_url + "ro_logistic/grid";
+	var site = base_url + "request_order_selected/grid";
 	$(document).ready(function(){
 	
 		newData = function (){
 			$('#dialog').dialog({
-				title: 'Tambah Barang',
+				title: 'Tambah RO Selected',
 				width: 380,
 				height: 240,
 				closed: true,
 				cache: false,
-				href: base_url+'ro_logistic/add',
+				href: base_url+'request_order_selected/add',
 				modal: true
 			});
 			 
 			$('#dialog').dialog('open');
-			url = base_url+'ro_logistic/save/add';
+			url = base_url+'request_order_selected/save/add';
 		}
 		// end newData
 		
@@ -23,17 +24,17 @@
 			// var row = $('#dg').datagrid('getSelected');
 			// if (row){
 				$('#dialog').dialog({
-					title: 'Edit Tambah',
+					title: 'Edit RO Selected',
 					width: 380,
 					height: 240,
 					closed: true,
 					cache: false,
-					href: base_url+'ro_logistic/edit/'+val,
+					href: base_url+'request_order_selected/edit/'+val,
 					modal: true
 				});
 				
 				$('#dialog').dialog('open');  
-				url = base_url+'ro_logistic/save/edit';
+				url = base_url+'request_order_selected/save/edit';
 			// }
 		}
 		//end editData
@@ -44,7 +45,7 @@
 				if(confirm("Apakah yakin akan menghapus data '" + val + "'?")){
 					var response = '';
 					$.ajax({ type: "GET",
-						 url: base_url+'ro_logistic/delete/' + val,
+						 url: base_url+'request_order_selected/delete/' + val,
 						 async: false,
 						 success : function(response){
 							var response = eval('('+response+')');
@@ -124,3 +125,31 @@
 		});
 		
 	});
+</script>
+<table id="dg" title="Kelola Data logistic" data-options="
+			rownumbers:true,
+			singleSelect:true,
+			autoRowHeight:false,
+			pagination:true,
+			pageSize:30,
+			fit:true,
+			">
+	<thead>
+		<tr>
+			<th field="user_id" sortable="true" width="150" hidden="true">Name 1</th>
+			<th field="nama_kategori" sortable="true" width="150">Name 2</th>
+			<th field="nama_sub_kategori" sortable="true" width="150">Name 3</th>
+			<th field="kode_barang" sortable="true" width="150">Name 4</th>
+
+			<th field="action" align="center" formatter="actionbutton" width="100">Aksi</th>
+		</tr>
+	</thead>
+</table>
+
+<!-- AREA untuk Form MENU >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  -->
+<div id="dialog-menu" class="easyui-dialog" style="width:400px;height:150px" closed="true" buttons="#dlg-buttons-menu">
+	<div id="dlg-buttons-menu">
+		<a href="#" class="easyui-linkbutton" iconCls="icon-ok" onclick="saveMenu()">Save</a>
+		<a href="#" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dialog-menu').dialog('close')">Cancel</a>
+	</div>
+</div>
