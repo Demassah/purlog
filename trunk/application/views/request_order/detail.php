368c464afc93a2a5b661503306a9e6ff306a0fef
Demassah@gmail.com <Demassah@gmail.com@71c10aaa-1455-b3bc-a450-06e7af52a3db>
2014-12-03 09:59:25 +0000

    js append

--- trunk/application/views/request_order/detail.php
+++ trunk/application/views/request_order/detail.php
@@ -4,22 +4,23 @@
 			<input type="hidden" name="kode" id="kode" value="<?=$kode?>">
 			<div class="fitem">
 				<label style="width:100px">Id Item</label>:
-				<input name="id_item" size="15" value="">
+				<input name="id_item" id="id_item" size="15" value="">
 			</div>
 			<div class="fitem" >
 				<label style="width:100px">Item Type</label>: 
-				<select id="id_kategori" name="id_kategori" style="width:200px;">
+				<select id="id_kategori" class="item_type" name="id_kategori" style="width:200px;">
 							<?=$this->mdl_prosedur->OptionKategori(array('value'=>$id_kategori));?>
 					</select>	
 			</div>
 			<div class="fitem">
 				<label style="width:100px">Item Name</label>:
-				<input name="id_item" size="15" value="">
+				<input name="id_item" size="15" id="item_name" value="">
 			</div>
 			<div class="fitem" >
 				<label style="width:100px">Description </label>: 
-				<textarea  name="kode_barang"></textarea>
+				<textarea  name="kode_barang" class="desc"></textarea>
 			</div>
+			<input type='button' name='tambah' class='button' id="tambah" value='Tambah' style="width:100px;">
 		</form>
 </div>
 <br />
@@ -35,20 +36,46 @@
 				">
 		<thead>
 			<tr>
-				<th field="user_id" sortable="true" width="150" hidden="true">ID</th>
-				<th field="id_type" sortable="true" width="150">Id Item</th>
-				<th field="item_type" sortable="true" width="150">Item Type</th>
-				<th field="item_name" sortable="true" width="150">Item Name</th>
-				<th field="description" sortable="true" width="150">Description</th>
-				<th field="action" align="center" formatter="actionbutton" width="140">Action</th>
+				<th width="150">ID Item</th>
+				<th width="150">Item Type</th>
+				<th width="150">Item Name</th>
+				<th width="150">Description</th>
 			</tr>
 		</thead>
-		<!--<thead>
-			<tr>
-				<th field="id_kategori" sortable="true" width="150" hidden="true">ID</th>
-				<th field="nama_kategori" sortable="true" width="350">Kategori</th>
-				<th field="action" align="center" formatter="actionbutton" width="100">Aksi</th>
-			</tr>
-		</thead>-->
+		<tbody id="data">
+			
+		</tbody>
 	</table>
 </div>
+
+<script type="text/javascript">
+	var arrayPenerimaan=[];
+		$("#tambah").click(function(){
+			var x = 0;
+			var id_item = $('#id_item').val();
+			var item_type = $('.item_type').val();
+			var item_name = $('#item_name').val();
+			var desc 			= $('.desc').val();
+			$.each( arrayPenerimaan, function( key, value ) {
+				if(id_item==value.id_item)
+				{
+					x = 1;
+				}
+			});
+			if(x==1){
+				alert('Kode barang tidak boleh sama');
+			}else{
+			$('#data').empty();
+			arrayPenerimaan.push({
+				id_item: id_item,
+				item_type: item_type, 
+				item_name: item_name,
+				desc:desc
+			});
+			
+			$.each( arrayPenerimaan, function( key, value ) {
+				$("#data").append("<tr class='gradeX'><td>" + value.id_item +"</td><td>" + value.item_type +"</td><td>" + value.item_name +"</td><td>" + value.desc +"</td></tr>");
+			});
+			}
+		});
+</script>
