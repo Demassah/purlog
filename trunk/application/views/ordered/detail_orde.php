449dc27fbd3a010a2c2e692244258dc3958fc967
demassah@gmail.com <demassah@gmail.com@71c10aaa-1455-b3bc-a450-06e7af52a3db>
2014-12-30 07:58:40 +0000

    penambahan menu ordered

---
+++ trunk/application/views/ordered/detail_orde.php
@@ -0,0 +1,52 @@
+ <h2 align='center'>Detail Vendor</h2>
+
+<div class="detail" style="margin:10px">
+<div class="fitem">
+		<label style="width:80px">ID Vendor </label>:
+			<?=$list->id_vendor;?>
+	</div>
+	<div class="fitem" >
+		<label style="width:80px">Vendor </label>:
+			<?=$list->name_vendor;?>
+	</div>
+	<div class="fitem">
+		<label style="width:80px">Contact </label>:
+			<?=$list->contact_vendor;?>
+	</div>
+	<div class="fitem">
+		<label style="width:80px">Mobile </label>:
+			<?=$list->mobile_vendor;?>
+	</div>
+	<div class="fitem">
+		<label style="width:80px">Address </label>:
+			<?=$list->address_vendor;?>
+	</div>
+	<div class="fitem">
+		<label style="width:80px">TOP </label>:
+			<?=$list->top;?>
+	</div>
+
+	<table class="tbl">
+		
+		<thead>
+			<tr>
+				<th>Kode barang</th>
+				<th>Nama Barang</th>
+				<th>Price</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php 
+			foreach ($item as $l) {
+				echo " <tr>
+				<td>".$l->kode_barang."</td>
+				<td>".$l->nama_barang."</td>
+				<td>".$l->price."</td>
+			</tr>
+				";
+			}
+		?>
+		</tbody>
+	</table>
+
+</div>
