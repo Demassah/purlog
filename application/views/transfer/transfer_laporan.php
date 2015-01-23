4bac799db2710b5a4b7211cd94bd9a41f9e182ef
iqbal4692 <Iqbal4692@gmail.com>
2015-01-23 09:28:35 +0000

    laporan & transfer

--- application/views/transfer/transfer_laporan.php
+++ application/views/transfer/transfer_laporan.php
@@ -16,7 +16,7 @@ else {
 	}
 	p{
 		margin:5px;
-		font-size: 20px;
+		font-size: 15px;
 	}
 	.header{
 		width:95%;
@@ -34,80 +34,90 @@ else {
 	.tblbtl td, 
 	.tblbtl th {
 		border-bottom: 1px solid black;
-		padding: 6px 6px;
+		padding: 4px 4px;
 		font-size: 13px;
 	}
 </style>
-
-<page backtop="10mm" backbottom="10mm" backleft="1mm" backright="3mm">
+<page backtop="15mm" backbottom="10mm" backleft="1mm" backright="3mm">
     <page_header>
         <table style="width: 100%;" class="header" cellspacing=0 cellpadding=0 >
             <tr>
-	
+              <!--   <td style="text-align: left;   width: 10%"><img src='<?php base_url()?>asset/images/logo.jpg' /></td>
+				<td style="text-align: center; width: 80%">
+					<h4><?php echo $namaUniv;?></h4>
+					<p><?php echo $alamatUniv;?></p>
+					<p><?php echo $kotaUniv;?></p>
+				</td>
+                <td style="text-align: right;   width: 10%"><?php //echo date('d/m/Y'); ?></td> -->
             </tr>
         </table>
     </page_header>
     <page_footer>
-        <table style="width: 100%; ">
+        <table style="width: 100%; border-top: solid 1px black;">
             <tr>
-                <td style="text-align: left;    width: 50%">PT. CIPAGANTI CITRA GRAHA </td>
+                <td style="text-align: left;    width: 50%">PT. Cipaganti Citra Graha </td>
                 <td style="text-align: right;    width: 50%">page [[page_cu]]/[[page_nb]]</td>
             </tr>
         </table>
     </page_footer>
 	<!--<table style="width:100%;">-->
-<p align="center"><b>Transfer Barang</b></p>
-<br><br>
+<h4 align="center"><b>Transfer List</b></h4>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
 	<!--<p align="center"><?php  echo $data_pdf[0]->nama_tahunajaran;?></p>-->
-	<table width= "100%"s>
+	<table>
 		<tr>
 			<td  align="left">ID Transfer</td>
-			<td  align="left"> : <?php  echo $data_pdf[0]->id_transfer;?></td>
-			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-			<td align="left">Date Create </td>
-			<td align="left"> : <?php  echo $data_pdf[0]->date_create;?></td>
+			<td  align="left"> : <?php echo $data_pdf[0]->id_transfer;?></td>
+			
+		</tr>    
+		<tr>
+			<td align="left">Date Create</td>
+			<td align="left"> : <?php echo $data_pdf[0]->date_create;?></td>
 		</tr>
 		<tr>
-			<td align="left">Tipe Transfer</td>
-			<td align="left"> : <?php  echo $data_pdf[0]->type_transfer;?></td>
-			<td>&nbsp;&nbsp;&nbsp;</td>
-			<td align="left">Requestor </td>
-			<td align="left"> : <?php  echo $data_pdf[0]->full_name;?></td>
+			<td align="left">Requestor</td>
+			<td align="left"> : <?php echo $data_pdf[0]->full_name;?></td>
 		</tr>
 	</table>
 	<br/> 
 	<div>
-	<table width= "100%" border="0.5" bordercolor="#000000" class='' cellspacing=0 cellpadding=0>
-	<tr>
-	<td width="80" align="center" border="" colspan="" rowspan="" headers=""><b>ID Detail</b></td>
-	<td width="80" align="center" colspan="" rowspan="" headers=""><b>ID Stock</b></td>
-	<td width="100" align="center" colspan="" rowspan="" headers=""><b>Kode Barang</b></td>
-	<td width="170" align="center" colspan="" rowspan="" headers=""><b>Nama Barang</b></td>
-	<td width="90" align="center" colspan="" rowspan="" headers=""><b>Qty Stock</b></td>
-	<td width="80" align="center" colspan="" rowspan="" headers=""><b>Qty Transfer</b></td>
-	<td width="80" align="center" colspan="" rowspan="" headers=""><b>Price</b></td>
-	<td width="140" align="center" colspan="" rowspan="" headers=""><b>Lokasi Stock</b></td>
-	<td width="140" align="center" colspan="" rowspan="" headers=""><b>Lokasi Transfer</b></td>
-	</tr>
-		<?php 
-			$no = 1;
-			foreach ($data_pdf as $data):
-		?>
-	<tr>
-	<td colspan="" align="center"  rowspan="" headers=""><? echo $data->id_detail_transfer; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->id_stock; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->kode_barang; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->nama_barang; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->qty_stock; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->qty; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->price; ?> </td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->lokasi_stock; ?></td>
-	<td colspan="" align="center" rowspan="" headers=""><? echo $data->id_lokasi; ?></td>
-	</tr>
-		<? 
-			$no++;
-			//foreach
-	 	 endforeach; ?>
+	<table width= "100%" border="0.5" bordercolor="#000000" class='tblbtl' cellspacing=0 cellpadding=0>
+	  <tr>
+		<th width="30" align="center">No</th>
+		<th width="70" align="center">ID Detail</th>
+		<th width="70" align="center">ID Stock</th>
+		<th width="90" align="center">Kode Barang</th>
+		<th width="165" align="center">Nama Barang</th>
+		<th width="50" align="center">Qty Stock</th>
+		<th width="50" align="center">Qty Transfer</th>
+		<th width="60" align="center">Price</th>
+		<th width="140" align="center">Lokasi Stock</th>
+		<th width="140" align="center">Lokasi Transfer</th>
+		<!--td width="65">Kredit</td-->
+	  </tr>
+	  <?php 
+		$no = 1;
+		foreach ($data_pdf as $data):
+	  ?>
+	  <tr>
+		<td align="center"><?php echo $no; ?></td>
+		<td align="center"><?php echo $data->id_detail_transfer;?></td>
+		<td align="center"><?php echo $data->id_stock;?></td>
+		<td align="center"><?php echo $data->kode_barang;?></td>
+		<td align="center"><?php echo $data->nama_barang;?></td>
+		<td align="center"><?php echo $data->qty_stock;?></td>
+		<td align="center"><?php echo $data->qty;?></td>
+		<td align="center"><?php echo $data->price;?></td>
+		<td align="center"><?php echo $data->lokasi_stock;?></td>
+		<td align="center"><?php echo $data->id_lokasi;?></td>
+	  </tr>
+	  <?php 
+		$no++;
+	//foreach
+	  endforeach; ?>
 	</table>
 	</div>
 </page>
