3c907fe68028564a9b7a6455d3f149d56fd70d89
iqbal4692 <Iqbal4692@gmail.com>
2015-02-09 02:44:43 +0000

    dr

--- application/controllers/document_receive.php
+++ application/controllers/document_receive.php
@@ -108,7 +108,8 @@ class document_receive extends CI_Controller {
 
         foreach($data['data_qty']['rows'] as $new) {
             $prev = $this->mdl_document_receive->getReceiveDetail   ($new['id_detail_receive'])->row();
-            if($prev->qty < $new['qty'] ||  $new['qty'] < 1) {
+            //if($prev->qty < $new['qty'] ||  $new['qty'] < 1) {
+            if($new['qty'] < 1) {
                 $error = true;
             }
         }
