cbddbc6f127cdfd20bb865212796b34ecbb372cf
Demassah <Demassah@gmail.com>
2015-02-10 02:37:31 +0000

    update sql dan inbound

--- application/views/inbound/form_add_detail.php
+++ application/views/inbound/form_add_detail.php
@@ -39,7 +39,7 @@
 							<span id="sisa_"'.$l->id_detail_pr.'">'.$l->sisa.'</span>
 							<input type="hidden" value="'.$l->sisa.'" id="sisa_'.$l->id_detail_pr.'" size="2">
 							<input type="text" name="sisa[]" value="" class="editbox" id="sisa_input_'.$l->id_detail_pr.'" size="2">
-							<input type="hidden" name="id_in[]" value="'.$l->id_in.'">
+							<input type="hidden" name="id_in[]" value="'.$l->id_in_asal.'">
 							</div>
 					</td>
 				</tr>
