e17d6539c655fc718d07d4246c876c076e46f17c
demassah@gmail.com <demassah@gmail.com@71c10aaa-1455-b3bc-a450-06e7af52a3db>
2015-03-11 10:06:35 +0000

    update laporan pembelian

---
+++ trunk/application/views/report_buy/buy_report_kode.php
@@ -0,0 +1,134 @@
+<?php
+if (empty($data_pdf)) {
+    echo "Tidak ada data";
+}
+else {
+?>
+<style>
+	h4 {
+		font-size:20px;
+		font-weight:bold;
+		margin:0px;
+		margin-bottom:5px;
+	}
+	page{
+		border:1px solid black;
+	}
+	p{
+		margin:5px;
+		font-size: 20px;
+	}
+	.header{
+		width:95%;
+		border-bottom:2px double black;
+		padding:10px 0;
+	}
+	.header img{
+		width:100px;
+		height:auto;
+	}
+	.tblbtl {
+		border: 1px solid black;
+		width: 100%;
+	}
+	.tblbtl td, 
+	.tblbtl th {
+		border-bottom: 1px solid black;
+		padding: 6px 6px;
+		font-size: 13px;
+	}
+</style>
+
+<page backtop="10mm" backbottom="10mm" backleft="1mm" backright="3mm">
+<h4 align="center"><b>Laporan Pembelian</b></h4>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+	<table>
+		<tr>
+			<td  align="left">Requestor</td>
+			<td  align="left"> : <?php echo $data_pdf[0]->full_name;?></td>
+			<td>&nbsp;&nbsp;</td>
+			<td align="left">External Doc No</td>
+			<td align="left"> : <?php echo $data_pdf[0]->ext_doc_no;?></td>
+		</tr>    
+		<tr>
+			<td align="left">Departement</td>
+			<td align="left"> : <?php echo $data_pdf[0]->departement_name;?></td>
+			<td>&nbsp;&nbsp;</td>
+			<td align="left">ETD</td>
+			<td align="left"> : <?php echo $data_pdf[0]->ETD;?></td>
+		</tr>
+		<tr>
+			<td align="left">Purpose</td>
+			<td align="left"> : <?php echo $data_pdf[0]->purpose;?></td>
+			<td>&nbsp;&nbsp;</td>
+			<td align="left">Date Create</td>
+			<td align="left"> : <?php echo tgl_indo(date('Y-m-d'))?></td>
+		</tr>
+		<tr>
+			<td align="left">Category Request</td>
+			<td align="left"> : <?php echo $data_pdf[0]->cat_req;?></td>
+		</tr>
+	</table>
+	<br/>
+<!-- <p align="center"><b>Laporan Pengiriman</b></p>
+		<table >
+		<tr>
+			<td  align="left">Requestor</td>
+			<td  align="left"> : <?php echo $data_pdf[0]->full_name;?></td>
+			<td>&nbsp;&nbsp;</td>
+			<td align="left">Departement</td>
+			<td align="left"> : <?php echo $data_pdf[0]->departement_name;?></td>
+			<td>&nbsp;&nbsp;</td>
+			<td align="left">Tanggal</td>
+			<td align="left"> : <?php echo tgl_indo(date('Y-m-d'));?></td>
+		</tr>    
+	</table>
+	<br/>  -->
+	<div>
+	<table width= "100%" border="0.5" bordercolor="#000000" class='' cellspacing=0 cellpadding=0>
+		<tr>
+			<td width="50" align="center" colspan="" rowspan="" headers=""><b>ID PO</b></td>
+			<td width="160" align="center" colspan="" rowspan="" headers=""><b>Supplier</b></td>
+			<td width="100" align="center" colspan="" rowspan="" headers=""><b>Kode Barang</b></td>
+			<td width="180" align="center" colspan="" rowspan="" headers=""><b>Nama Barang</b></td>
+			<td width="60" align="center" colspan="" rowspan="" headers=""><b>Qty</b></td>
+			<td width="150" align="center" colspan="" rowspan="" headers=""><b>Harga</b></td>
+			<td width="150" align="center" colspan="" rowspan="" headers=""><b>Total</b></td>
+			<td width="180" align="center" colspan="" rowspan="" headers=""><b>Date Create</b></td>
+		</tr>
+		<?php 
+			$no = 1;
+			$total = 0;
+			foreach ($data_pdf as $data):
+		?>
+		<tr>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo $data->id_po; ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo $data->name_vendor; ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo $data->kode_barang; ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo $data->nama_barang ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo $data->qty; ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo "Rp." . number_format($data->price,2,',','.'); ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo "Rp." . number_format($data->total,2,',','.'); ?></td>
+			<td colspan="" rowspan=""  align="center" headers=""><? echo  date('d-m-Y',strtotime($data->date_create)); ?></td>
+			
+		</tr>
+		<? 
+			$total+=$data->total;
+			$no++;
+			//foreach
+	 	 endforeach; ?>
+	 	 <tr>
+	 		
+	 	 		<td colspan="6" rowspan=""  align="center" headers="">Total</td>
+	 	 		<td colspan="2" rowspan=""  align="center" headers=""><strong><? echo "Rp." . number_format($total,2,',','.');?></strong></td>
+	 	 </tr>
+	</table>
+		<br><br>
+	<div class="class="footer"">
+	</div>
+	</div>
+</page>
+<?php } ?>
---
+++ trunk/application/views/report_buy/index.php
@@ -0,0 +1,140 @@
+<div id="toolbar_report_buy" style="padding:5px;height:auto">
+	<div style="margin-bottom:5px">		
+	</div>
+	<div class="fsearch">
+		<table width="670" border="0">
+		  <tr>
+			<td>Search By Date</td>
+			<td>: 
+				<input class="date" name="date_1"  id="date_1" size="15">
+			</td>
+			<td>To</td>
+			<td>
+				<input name="date_2" class="date" id="date_2" size="15">
+			</td>
+			<td><a href="#" onclick="filter()" class="easyui-linkbutton" iconCls="icon-search">Search</a></td>
+			<td></td>
+			
+		  </tr>
+		  <tr>
+		  	<td>Supplier</td>
+		  	<td>:
+		  		<input name="supplier"  id="supplier" size="15">
+		  	</td>
+		  	<td>Kode Barang</td>
+		  	<td>
+		  		<input name="kode_barang"  id="kode_barang" size="15">
+		  	</td>
+		  	<td><a href="#" onclick="reset()" class="easyui-linkbutton" iconCls="icon-reload">Reset</a></td>
+		  </tr>
+
+		</table>
+	</div>
+</div>
+
+<table id="dg_report_buy" title="Report Delivery Order" data-options="
+			rownumbers:true,
+			singleSelect:true,
+			autoRowHeight:false,
+			pagination:true,
+			pageSize:30,
+			fit:true,
+			toolbar:'#toolbar_report_buy'
+			">
+	<thead>
+		<tr>
+			<th field="" sortable="true" width="150" hidden="true"></th>
+			<th field="id_po" sortable="true" width="50" >ID PO</th>
+			<th field="name_vendor" sortable="true" width="130">Supplier</th>
+			<th field="kode_barang" sortable="true" width="130">Kode Barang</th>
+			<th field="nama_barang" sortable="true" width="180">Nama Barang</th>
+			<th field="qty" sortable="true" width="120">Qty</th>
+			<th field="price" sortable="true" width="120">price</th>
+			<th field="total" sortable="true" width="120">Total</th>
+			<th field="date_create" sortable="true" width="180">Date Create</th>
+
+	
+		</tr>
+	</thead>
+</table>
+
+<script>
+	var url;
+	$(document).ready(function(){
+
+	
+		$('.date').datepick({dateFormat: 'yyyy-mm-dd'});
+
+		// reset 
+		reset = function(){
+			$('#date_1').val('');
+			$('#date_2').val('');
+			$('#supplier').val('');
+			$('#kode_barang').val('');
+			$('#dg_report_buy').datagrid('load',{
+				tgl : $('#date_create').val(),
+				
+				
+			});
+		}
+
+		filter = function(){
+			$('#dg_report_buy').datagrid('load',{
+				date_1 : $('#date_1').val(),
+				date_2 : $('#date_2').val(),
+				supplier : $('#supplier').val(),
+				kode_barang : $('#kode_barang').val()
+			});
+			//$('#dg').datagrid('enableFilter');
+		}
+
+    ExportPdf = function(val){
+			var date_1 = $('#date_1').val();
+			var date_2 = $('#date_2').val();
+			var supplier = $('#supplier').val();
+			var kode_barang = $('#kode_barang').val();
+      		if (date_1 != '' && date_2 != ''){
+				window.open('<?=base_url().'laporan_pembelian/laporan_pdf/'?>'+date_1+'/'+date_2);
+			}else{
+				if(supplier !=''){
+					window.open('<?=base_url().'laporan_pembelian/laporan_pdf_supp/'?>'+supplier);
+				}else{
+					if(kode_barang !=''){
+						window.open('<?=base_url().'laporan_pembelian/laporan_pdf_kode/'?>'+kode_barang);
+					}else{
+						$.messager.show({
+						title: 'Warning',
+						msg: 'Harap Isi Filter Terlabih Dahulu'
+						});
+					}
+				}
+			}
+    }
+
+
+		$(function(){
+			$('#dg_report_buy').datagrid({
+				url:"<?=base_url()?>laporan_pembelian/grid"
+			});
+		});
+		
+		//# Tombol Bawah
+		$(function(){
+			var pager = $('#dg_report_buy').datagrid().datagrid('getPager');	// get the pager of datagrid
+			pager.pagination({
+				buttons:[
+				<?if($this->mdl_auth->CekAkses(array('menu_id'=>54, 'policy'=>'PDF'))){?>
+					{
+						iconCls:'icon-pdf',
+						text:'Export PDF',
+						handler:function(){
+							ExportPdf();
+						}
+					}
+				<?}?>		
+				]
+			});			
+		});
+		
+	});
+</script>
